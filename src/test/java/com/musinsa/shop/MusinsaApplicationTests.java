cf475041fd6a50be9dc1f7010c2016d137aa3c66
sdgranger <sdgranger@daum.net>
2024-05-23 20:58:09 +0900

    init

---
+++ src/test/java/com/musinsa/shop/MusinsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
