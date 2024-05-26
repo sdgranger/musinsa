46a43d348c396ed231b59abd4952f94d7e2e0680
sdgranger <sdgranger@daum.net>
2024-05-26 23:19:24 +0900

    patch swagger BrandProductController

--- src/main/java/com/musinsa/shop/controller/BrandProductController.java
+++ src/main/java/com/musinsa/shop/controller/BrandProductController.java
@@ -4,6 +4,8 @@ import com.musinsa.shop.controller.request.BrandProductAddRequest;
 import com.musinsa.shop.controller.request.BrandProductChangeRequest;
 import com.musinsa.shop.controller.request.BrandProductRemoveRequest;
 import com.musinsa.shop.service.BrandProductCrudService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +14,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/brand/products")
+@Tag(name = "브랜드, 상품을 추가, 삭제, 수정 API")
 public class BrandProductController {
 
     private final BrandProductCrudService brandProductCrudService;
 
     @PostMapping
+    @Operation(summary = "브랜드, 상품을 추가", description = "")
     public ResponseEntity<Void> addProduct(@RequestBody @Valid BrandProductAddRequest productAddRequest) {
         brandProductCrudService.add(productAddRequest.toBrandProductAdd());
 
@@ -24,6 +28,7 @@ public class BrandProductController {
     }
 
     @PutMapping
+    @Operation(summary = "브랜드 상품 정보를 수정", description = "")
     public ResponseEntity<Void> updateProduct(@RequestBody @Valid BrandProductChangeRequest productUpdateRequest) {
         brandProductCrudService.update(productUpdateRequest.toBrandProductChange());
 
@@ -31,6 +36,7 @@ public class BrandProductController {
     }
 
     @DeleteMapping
+    @Operation(summary = "브랜드, 상품 삭제", description = "")
     public ResponseEntity<Void> deleteProduct(@RequestBody @Valid BrandProductRemoveRequest productDeleteRequest) {
         brandProductCrudService.delete(productDeleteRequest.getBrandName());
 
